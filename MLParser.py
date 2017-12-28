7d94fed71cbe75e2f88b003fe916b8efb274f1e0
Thiago Lima <thiagocalima@users.noreply.github.com>
2017-12-28 11:49:02 -0200

    Update MLParser.py

--- MLParser.py
+++ MLParser.py
@@ -79,7 +79,7 @@ def main():
 	
 	try:
 		print('Mode: ' + str(round(mode(adPrices))))
-	except statistics.exceptions.StatisticsError as m:
+	except StatisticsError as m:
 		print('ERROR: Unable to print Mode!')
 
 	print('Median: ' + str(round(median(adPrices))))
